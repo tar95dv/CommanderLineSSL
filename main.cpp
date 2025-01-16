2df89da199ca409169a19e5d161350d447fc4054
tar95dv <tar95dv@yandex.ru>
2025-01-16 12:22:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 The MIT License
 
-Copyright (c) 2019 Tar95DV.
+Copyright (c) 2025 Tar95DV.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
